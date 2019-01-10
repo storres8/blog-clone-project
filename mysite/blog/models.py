ed3166f57beafc0014a86642620bf3bb289083de
storres8 <stevetorres7562@gmail.com>
2019-01-10 17:31:38 -0500

    built Comment model.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -19,3 +19,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=256)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approve_comments = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comments = True
+        self.save()
+
+    def __str__(self):
+        return self.text
