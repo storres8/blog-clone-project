c743347d9fd00934acf3e5765c3343c2e0dcfb27
storres8 <stevetorres7562@gmail.com>
2019-01-21 22:07:25 -0500

    updated urls and edited templates.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<slug:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', views.PostCreateView.as_view(), name='post_view'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
     path('post/<slug:pk>/edit', views.PostUpdateView.as_view(), name='post_update'),
-    path('drafts/'views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<slug:pk>/remove',views.PostDeleteView.as_view(), name='post_remove')
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<slug:pk>/comment', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comments/<slug:pk>/approve', views.comment_approve, name='comment_approve'),
+    path('comments/<slug:pk>/remove', views.comment_remove, name='comment_remove'),
+    path('post/<slug:pk>/publish',views.post_publish, name='post_publish'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
@@ -10,9 +11,11 @@ from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
+
 
 
 class AboutView(TemplateView):
@@ -51,4 +54,40 @@ class DraftListView(LoginRequiredMixin, DeleteView):
     model = Post
 
     def get_queryset(self):
-        return Post.object.filter(published_date__isnull= True).order_by('created_date')
+        return Post.object.filter(published_date__isnull= True).order_by('-created_date')
+
+###################################
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
